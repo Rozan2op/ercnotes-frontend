a566cbc11307a5541996006953f65cff2b57a549
Rozan Shah <rozan.rozan2222@gmail.com>
2026-01-28 12:21:57 +0545

    updating after running node scan pdfs

--- manual_data.js
+++ manual_data.js
@@ -6,5 +6,34 @@ window.manualResources = {
             "name": "C_Past",
             "link": "pdf/computer/1/Computer Programming/Past Question/C_Past.pdf"
         }
+    ],
+    "Digital Control System": [
+        {
+            "type": "Manual",
+            "name": "Manual DCS",
+            "link": "pdf/electrical/6/Digital Control System/Manual/Manual DCS.pdf"
+        },
+        {
+            "type": "Past Question",
+            "name": "Old Question DCS",
+            "link": "pdf/electrical/6/Digital Control System/Past Question/Old Question DCS.pdf"
+        },
+        {
+            "type": "Syllabus",
+            "name": "syllabusDCS",
+            "link": "pdf/electrical/6/Digital Control System/Syllabus/syllabusDCS.pdf"
+        }
+    ],
+    "Signal Analysis": [
+        {
+            "type": "Past Question",
+            "name": "Old Question SA",
+            "link": "pdf/electrical/6/Signal Analysis/Past Question/Old Question SA.pdf"
+        },
+        {
+            "type": "Past Question",
+            "name": "Past Question Signal Analysis",
+            "link": "pdf/electrical/6/Signal Analysis/Past Question/Past Question Signal Analysis.pdf"
+        }
     ]
 };
